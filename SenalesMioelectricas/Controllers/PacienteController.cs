b866b1e412212365ce76494fe5c55e2430079532
oapm1996 <isc.oscarpina@gmail.com>
2023-02-16 21:00:05 -0800

    Avance controladores (CRUD metodos)

--- SenalesMioelectricas/Controllers/PacienteController.cs
+++ SenalesMioelectricas/Controllers/PacienteController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SenalesMioelectricas.Models;
@@ -9,22 +13,116 @@ namespace SenalesMioelectricas.Controllers
 {
     public class PacienteController : Controller
     {
-        // GET: Paciente
-        public ActionResult Index()
+        private BDSenalesMioelectricasEntities db = new BDSenalesMioelectricasEntities();
+
+        // GET: Pacientes
+        public async Task<ActionResult> Index()
         {
-            List<PacienteCLS> pacienteList = null;
-            using (var bd = new BDSenalesMioelectricasEntities())
+            return View(await db.Pacientes.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
             {
-                pacienteList = (from paciente in bd.Pacientes
-                                select new PacienteCLS
-                                { 
-                                    Nombre = paciente.Nombre,
-                                    Apellido_Paterno = paciente.Apellido_Paterno,
-                                    Apellido_Materno = paciente.Apellido_Materno,
-                                    Sexo = paciente.Sexo
-                                }).ToList();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-                return View(pacienteList);
+            Paciente paciente = await db.Pacientes.FindAsync(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "StudentId,StudentName,Marks")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pacientes.Add(paciente);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(paciente);
+        }
+
+        // GET: Paciente/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = await db.Pacientes.FindAsync(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "StudentId,StudentName,Marks")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        // GET: Paciente/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = await db.Pacientes.FindAsync(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Paciente student = await db.Pacientes.FindAsync(id);
+            db.Pacientes.Remove(student);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
